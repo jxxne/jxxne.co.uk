43df572140bf2bb5e7d085fb9c0531e1b7d19668
jane <110698553+jxxne@users.noreply.github.com>
2024-09-12 19:11:19 +0100

    basic webserver + a potentially used loading screen
    nothing rlly here.

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+const express = require('express') 
+const path = require('path') 
+const hbs = require('hbs') 
+const app = express() 
+  
+// View Engine Setup 
+app.set('views', path.join(__dirname)) 
+app.set('view engine', 'hbs') 
+app.use(express.static('public'))
+
+  
+app.get('/', function(req, res){ 
+    res.render('Home', { 
+       array: ['One', 'Two', 'Three', 'Four'], 
+       message: 'Greetings from geekforgeeks'
+    }) 
+}) 
+  
+app.listen(8080, function(error){ 
+    if(error) throw error 
+    console.log("Server created Successfully") 
+}) 
