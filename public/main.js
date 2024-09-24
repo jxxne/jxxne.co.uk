f243b7be730794e8b5dd18c3031a7fed07f40e5b
jane <110698553+jxxne@users.noreply.github.com>
2024-09-24 09:20:53 +0100

    Switched visibility to classes instead of style tags, allows for (and added) fade in.

--- public/main.js
+++ public/main.js
@@ -2,14 +2,11 @@ function revealWindow(pageId) {
     const pages = document.querySelectorAll("div.content");
 
     for(let i=0; i < pages.length; i++) {
-        if(pages[i].style.visibility == 'visible') {
-            pages[i].style.visibility = 'hidden';
-            pages[i].style.opacity = 0;
-
+        if(!pages[i].classList.contains("hidden")) {
+            pages[i].classList.add("hidden");
             // NOTE: currently i want to add a fade animation, and this won't allow for that, as it's immediently both disabling visibility and setting the opacity to 0.
             // the solution is to transition the opacity, then remove visibility.
         }
     }
-    document.getElementById(pageId).style.visibility = 'visible';
-    document.getElementById(pageId).style.opacity = 1;
+    document.getElementById(pageId).classList.remove("hidden");
 }
