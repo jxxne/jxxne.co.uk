6268c3b8778865187207a4910490d6fee2bf0e55
jane <110698553+jxxne@users.noreply.github.com>
2024-09-28 16:54:08 +0100

    Added functionality to "what i'm listening to section" through last.fm (with caveats, read description)
    Last.fm's API won't let me get any album artwork, for no apparent reason. I don't know why, I hope it's just me doing something wrong that can easily be fixed, but I've tried most things and it's not been able to get artwork, even just from a web browser using the API.

--- public/main.js
+++ public/main.js
@@ -13,4 +13,11 @@ $.getJSON('../api/getTopTracks', function(data) {
     document.getElementById("abouttrackname").innerHTML = document.getElementById("abouttrackname").innerHTML.replace("INSERTTRACKHERE", data["toptracks"]["track"][0]["artist"]["name"].toLowerCase() + " - " + data["toptracks"]["track"][0]["name"].toLowerCase())
     document.getElementById("abouttrackname").setAttribute("href",data["toptracks"]["track"][0]["url"]);
     console.log(data["toptracks"]["track"][0]["artist"]["name"].toLowerCase() + " - " + data["toptracks"]["track"][0]["name"].toLowerCase());
+
+    for (let i = 0; i < 10; i++) {
+        document.getElementById("listening").innerHTML = document.getElementById("listening").innerHTML + '<div class="item">             <img src="' + data["toptracks"]["track"][0]["image"][1]["#text"] + '" class="backgroundimage">             <img style="z-index: 5;" src="' + data["toptracks"]["track"][i]["image"][1]["#text"] + '">             <p class="artistname">'+ data["toptracks"]["track"][i]["artist"]["name"].toLowerCase() +'</p>             <p class="trackname">'+ data["toptracks"]["track"][i]["name"].toLowerCase() +'</p>             <p class="position">' + (i+1) + '</p>           </div>';
+        console.log(i);
+      }
 });
+
+console.log
